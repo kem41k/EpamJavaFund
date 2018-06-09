611316ccf5324517c08ee0d1037e749e228493f0
Kamil Yagafarov <kem41k@gmail.com>
2018-06-09 21:25:00 +0300

    1.02 is done

---
+++ HW_1/HW_1.02/Sequence.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Sequence {
+    public static void main(String[] args) {
+        double eps = getEpsilon();
+        double element = 1;
+        int n = 1;
+        while(element >= eps){
+            element = 1 / Math.pow(n + 1, 2);
+            System.out.println("a[" + n++ + "] = " + element);
+        }
+        System.out.println("Answer: n = " + --n);
+    }
+
+    public static double getEpsilon() {
+        double input = -1;
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a double value from (0;1) interval:");
+        while (true){
+            try {
+                System.out.print("eps = ");
+
+                input = Double.parseDouble(bufferRead.readLine());
+                if (input > 0 && input < 1)
+                    break;
+                else
+                    System.out.println("Wrong input! Epsilon should be from (0;1) interval!\n");
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+            catch (NumberFormatException numEx) {
+                System.out.println("Wrong input! Epsilon should be a double value!\n");
+            }
+        }
+        return input;
+    }
+}
