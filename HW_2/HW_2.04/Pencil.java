e8e6decf5fb9deae59dadc545776177e888bb4a1
kem41k <kem41k@gmail.com>
2018-06-18 02:15:26 +0300

    4 is done

---
+++ HW_2/HW_2.04/Pencil.java
@@ -0,0 +1,30 @@
+public class Pencil extends WritingTool{
+    public enum Hardness {
+        B,
+        HB,
+        F,
+        H,
+        H2
+    }
+
+    private Hardness hardness;
+    private boolean hasEraser;
+
+    private Pencil(String name, double price, String color, Hardness hardness, boolean hasEraser) {
+        super(name, price, color);
+        this.hardness = hardness;
+        this.hasEraser = hasEraser;
+    }
+
+    public static Pencil createPencil(String name, double price, String color, Hardness hardness, boolean hasEraser) {
+        if (price < 0) {
+            System.out.println("ERROR! Price can not be negative number!");
+            return null;
+        }
+        return new Pencil(name, price, color, hardness, hasEraser);
+    }
+
+    public String getInfo() {
+        return String.format("%-25s: $%.2f, color: %s, hardness: %s, eraser: %b", getClass().getName() + "\"" + this.getName() + "\"", this.getPrice(), this.getColor(), hardness, hasEraser);
+    }
+}
